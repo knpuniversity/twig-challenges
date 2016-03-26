4bf23113eb0fc2c5baf5ca7a39d4adfe599be369
Ryan Weaver <ryan@thatsquality.com>
2016-03-26 12:33:55 -0400

    Adding twig/twig and a basic rendering file

---
+++ render.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+/*
+ * Basic Twig setup, that will be modified by future steps
+ */
+$loader = new \Twig_Loader_Filesystem([
+    __DIR__
+]);
+$twig = new \Twig_Environment([$loader], [
+    'debug' => true
+]);
